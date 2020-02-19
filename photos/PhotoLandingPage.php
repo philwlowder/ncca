792885e826f8389648e2a36324929403182cac4f
philwlowder <lowder.phil@gmail.com>
2020-02-18 20:06:28 -0500

    upload photos folder
    first

---
+++ photos/PhotoLandingPage.php
@@ -0,0 +1,75 @@
+<html>
+<head>
+
+<title>North Carolina Chess Association</title>
+
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+<meta http-equiv="pragma" content="no-cache">
+<link rel="stylesheet" href="../website.css" type="text/css">
+
+
+</head>
+
+<body bgcolor="#CCFFFF" text="#000000" >
+<table style="width: 80%; height: 933px;" border="0" cellspacing="0" cellpadding="0">
+<tbody>
+<tr>
+<td rowspan="2" width="143" height="933" valign="top"><a title="I Like Calling North Carolina HOME!!" href="http://www.ncchess.org/index.php"><img src="../ncflag.jpg" border="0" alt="" width="130" height="86" /></a>
+<p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</p>
+
+
+
+<?PHP
+include("../includes/include_links.php");
+?>
+
+   </td>
+    <td align="left" valign="top" width="709" height="85"> 
+      <p align="center"> 
+      <img src="../ncca.gif" width="558" height="83">
+    </td>
+  </tr>
+  <tr> 
+    <td width="709" valign="top" height="609"> 
+      <h4 align="center">&nbsp;</h4>
+      <h4 align="center"><b><font color="#FF0000" face="Arial" size="5">PHOTOS</font></b></h4>
+      <O:P>
+      <p align="left">&nbsp;</p>
+      <p align="left">
+      <O:P>
+        <b><font face="Arial">
+      <a title="The Camera Adds about 20 Rating Points!" href="Lots2009.htm">Land of the Sky XXII:&nbsp; Photographic Memories 2009</a></font></b></p>
+      <p align="left">
+      <O:P>
+        <b><font face="Arial">
+      <a title="Speed Limit:  65 pawns per hour" href="Lots09Bz.htm">Land of the Sky XXII:&nbsp; 
+      <i>Speed Chess Tournament 2009</i></a></font></b><O:P></p>
+      <p align="left"><b><a href="http://www.ncchess.org/Photos/JodreyGQ.htm">
+      <font face="Arial">Mock
+      GQ Cover Says that Chess Players are Just too Sexy</font></a></b></p>
+      <p align="left"><b><font face="Arial"><a href="http://www.ncchess.org/Photos/NCOpenPhoto.htm">Action
+      During the 2007 N.C. Open in Greensboro, Nov. 2-4&nbsp;&nbsp;</a></font></b></p>
+      <p align="left"><b><font face="Arial"><a href="http://www.ncchess.org/Photos/Schroer.jpg">IM
+      Jonathan Schroer in Action in a Carolina Cobras Match</a></font></b></p>
+      <p align="left"><b><font face="Arial"><a href="http://www.ncchess.org/Photos/ChrisMabe.htm">2007 World Open Under
+      2200 Champ NM Chris Mabe</a></font></b></p>
+      <p align="left"><b><font face="Arial"><a href="http://www.ncchess.org/Photos/Chuma.jpg">2007
+      N.C. Invitational Winner Andrey Chumachenko</a></font></b></p>
+      <p align="left"><b><a href="Picture1.jpg"><font face="Arial">2006
+      Invitational: FM Ron Simpson (near) shows a game ending to NM Neal Harris</font></a></b></p>
+      <p align="left"><b><font face="Arial" size="3"><a href="15.jpg">2005
+      Denker, Polgar Participants Laura Lee, Nelson Lopez II, and Amelia
+      Wheeless</a></font></b></p>
+      <p align="left"><b><font face="Arial" size="3"><a href="CraigJonesJPG.jpg">2005
+      and 2006 N.C. Invitational Winner Craig Jones</a></font></b></p>
+      <p align="center">&nbsp;</p>
+      <p><b><font face="Rockwell">Copyright &copy; 2001-09, North Carolina Chess 
+        Association</font></b></p>
+	<p><font face="Century Gothic"><br>
+      </font>
+      </p>
+      </td>
+  </tr>
+</table>
+</body>
+</html>
